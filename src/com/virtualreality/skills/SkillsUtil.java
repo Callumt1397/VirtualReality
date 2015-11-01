a26b20404bc653a16f2a7e420c64eb0249d121ed
nerdtron123 <mcmatters123@gmail.com>
2015-11-01 13:33:32 -0500

    Update skillsutil

--- src/com/virtualreality/skills/SkillsUtil.java
+++ src/com/virtualreality/skills/SkillsUtil.java
@@ -1,9 +1,20 @@
 package com.virtualreality.skills;
 
+import java.io.File;
+
+import org.bukkit.entity.EntityType;
+
+import com.virtualreality.Config;
+import com.virtualreality.VirtualReality;
+
 public class SkillsUtil {
 
 	public static double toDouble(String[] args){
 		return Double.parseDouble(args[0] + args[1]);
+	}
+	
+	public static double getEntityKillValue(EntityType type, Skill s) {
+		return new Config(new File("skills"), s.name().toLowerCase(), VirtualReality.getInstance()).getConfig().getDouble(type.toString().toLowerCase(),0);
 	}
 	
 }
--- src/com/virtualreality/skills/listeners/SwordSkillListener.java
+++ src/com/virtualreality/skills/listeners/SwordSkillListener.java
@@ -1,23 +1,16 @@
 package com.virtualreality.skills.listeners;
 
-import java.io.File;
-
 import org.bukkit.Material;
-import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDeathEvent;
 
-import com.virtualreality.Config;
 import com.virtualreality.User;
-import com.virtualreality.VirtualReality;
 import com.virtualreality.skills.Skill;
 import com.virtualreality.skills.SkillsUtil;
 
 public class SwordSkillListener implements Listener {
-
-	private Config config = new Config(new File("skills"), "swords", VirtualReality.getInstance());
 
 	@EventHandler
 	public void onEntityDeath(EntityDeathEvent event) {
@@ -27,13 +20,9 @@ public class SwordSkillListener implements Listener {
 			if (hand == Material.WOOD_SWORD || hand == Material.STONE_SWORD || hand == Material.IRON_SWORD
 					|| hand == Material.DIAMOND_SWORD) {
 					User user = new User(event.getEntity().getKiller());
-					user.setSkillLevelDouble(Skill.SWORDS, SkillsUtil.toDouble(user.getSkillLevel(Skill.SWORDS)) + getEntityKillValue(event.getEntityType()));
+					user.setSkillLevelDouble(Skill.SWORDS, SkillsUtil.toDouble(user.getSkillLevel(Skill.SWORDS)) + SkillsUtil.getEntityKillValue(event.getEntityType(), Skill.SWORDS));
 			}
 		}
-	}
-
-	public double getEntityKillValue(EntityType type) {
-		return config.getConfig().getDouble(type.toString().toLowerCase(),0);
 	}
 
 }
